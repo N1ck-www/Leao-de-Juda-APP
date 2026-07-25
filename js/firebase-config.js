5b4d1ea400febd85bb99955046d567345574af9a
N1ck-www <nicolaskaka33@gmail.com>
2026-07-25 09:44:49 -0300

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcpwnsTOWt1hyl_mtFH8MLfE2TtZT_2jI",
+  authDomain: "leao-dejuda.firebaseapp.com",
+  projectId: "leao-dejuda",
+  storageBucket: "leao-dejuda.firebasestorage.app",
+  messagingSenderId: "945551733111",
+  appId: "1:945551733111:web:0e98d170a9f49ee3d9f172"
+};
